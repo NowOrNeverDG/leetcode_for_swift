4b19604fbd74946a258ac58da55e9c2191d03b88
Ge Ding <447405890@qq.com>
2022-01-22 23:50:48 -0500

    update

--- Algorithm/Search-DFS.playground/Contents.swift
+++ Algorithm/Search-DFS.playground/Contents.swift
@@ -1,8 +1,60 @@
+import Darwin
+//417. Pacific Atlantic Water Flow
+//Input: heights = [[1,2,2,3,5],[3,2,3,4,4],[2,4,5,3,1],[6,7,1,4,5],[5,1,1,2,4]]
+//Output: [[0,4],[1,3],[1,4],[2,2],[3,0],[3,1],[4,0]]
+func pacificAtlantic(_ heights: [[Int]]) -> [[Int]] {
+    if heights.count == 0 { return []}
+    
+    let directions:[[Int]] = [[0,1],[0,-1],[1,0],[-1,0]]
+    for m in 0..<heights.count {
+        dfs(heights: &heights, row: <#T##Int#>, column: 0)
+    }
+    
+    
+    
+    
+    func dfs(heights:inout [[Int]], row:Int, column:Int) {
+        
+    }
+}
+
+
+
+//130. Surrounded Regions
+//Input: board = [["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]
+//Output: [["X","X","X","X"],["X","X","X","X"],["X","X","X","X"],["X","O","X","X"]]
+func solve(_ board: inout [[Character]]) {
+    if board.count == 0 { return }
+    let directions:[[Int]] = [[0,1],[0,-1],[1,0],[-1,0]]
+    for m in 0..<board.count {
+        dfs(board: &board, row: m, column: 0)
+        dfs(board: &board, row: m, column: board[0].count-1)
+    }
+    for n in 0..<board[0].count {
+        dfs(board: &board, row: 0, column: n)
+        dfs(board: &board, row: board.count-1, column: n)
+    }
+    for m in 0..<board.count {
+        for n in 0..<board[0].count {
+            if board[m][n] == "T" {board[m][n] = "O"}
+            else if board[m][n] == "O" {board[m][n] = "X"}
+        }
+    }
+    
+    func dfs(board:inout [[Character]], row:Int, column:Int){
+        if row < 0 || row >= board.count || column < 0 || column >= board[0].count || board[row][column] != "O" { return }
+        
+        board[row][column] = "T"
+        for dir in directions {
+            dfs(board: &board, row: row+dir[0], column: column+dir[1])
+        }
+    }
+}
+
+
 //547. Number of Provinces
 //Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
 //Output: 2
-
-import Darwin
 func findCircleNum(_ isConnected: [[Int]]) -> Int {
     if isConnected.count == 0 { return 0 }
     
