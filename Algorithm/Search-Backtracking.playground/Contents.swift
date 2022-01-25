9134ef6de131e0cb20718b304248d18f3fe2decd
Ge Ding <447405890@qq.com>
2022-01-25 18:49:34 -0500

    update 17

--- Algorithm/Search-Backtracking.playground/Contents.swift
+++ Algorithm/Search-Backtracking.playground/Contents.swift
@@ -1,22 +1,34 @@
 import UIKit
+import Darwin
 
 //17. Letter Combinations of a Phone Number
 //Input: digits = "23"
 //Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 func letterCombinations(_ digits: String) -> [String] {
-    let keyMaps:[Character:String] = ["2":"abc","3":"def","4":"ghi","5":"jki","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"]
+    if digits.count == 0 {return []}
     
-    var value = [String]()
-    for nu in digits {
-        if let s = keyMaps[nu] {
-            value.append(s)
+    
+    let keyMaps:[Character:String] = ["2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"]
+    var result = [String]()
+    var combinations = [String]()
+    
+    for char in digits {
+        if let string = keyMaps[char] { combinations.append(string)}
+    }
+    backtracking(current: "",combinations:combinations, index:0,result: &result)
+    
+    func backtracking(current:String,combinations:[String],index:Int,result: inout [String]) {
+        if current.count == combinations.count {
+            result.append(current)
+            return
+        }
+        for char in combinations[index] {
+            backtracking(current: current+String(char), combinations: combinations, index: index+1, result: &result)
         }
     }
-    
-    var result = [String]()
-    
-    
     return result
 }
+
+
     
     
