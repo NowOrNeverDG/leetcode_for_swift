9f5ffa90ac88546fb5978be669926e0d0c3a5bd3
Ge Ding <447405890@qq.com>
2022-02-26 18:26:52 -0500

    update 121/406

--- Algorithm/Greedy.playground/Contents.swift
+++ Algorithm/Greedy.playground/Contents.swift
@@ -1,11 +1,50 @@
 import UIKit
 import Darwin
 
+//121. Best Time to Buy and Sell Stock
+//Input: prices = [7,1,5,3,6,4]
+//Output: 5
+func maxProfit(_ prices: [Int]) -> Int {
+    if prices.count == 0 {return 0}
+    
+}
+
+
+//406. Queue Reconstruction by Height
+//Input:[[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+//Output:[[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+func reconstructQueue(_ people: [[Int]]) -> [[Int]] {
+    if people.count == 0 {return people}
+    
+    let input = people.sorted { x, y in
+        if x[0] == y[0]  {return x[1] < y[1]}
+        return x[0] > y[0]
+    }
+    var result:[[Int]] = Array()
+    for i in 0..<input.count {
+        result.insert(input[i], at: input[i][1])
+    }
+    return result
+}
+
+//452. Minimum Number of Arrows to Burst Balloons
 //Input: points = [[10,16],[2,8],[1,6],[7,12]]
 //Output: 2
-
 func findMinArrowShots(_ points: [[Int]]) -> Int {
-        
+    if points.count == 0 {return 0}
+     
+    let input = points.sorted { x, y in
+        return x[1] < y[1]
+    }
+    
+    var result = 1
+    var end = input[0][1]
+    for i in 0..<input.count {
+        if input[i][0] <= end {continue}
+        end = input[i][1]
+        result += 1
+    }
+    return input.count - result
 }
 
 
