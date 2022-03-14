10418a2da0fe9705522787f34c6e97a23ef922cd
Ge Ding <447405890@qq.com>
2022-03-14 14:43:17 -0400

    update hackrank

--- HackRank/HackRank.playground/Contents.swift
+++ HackRank/HackRank.playground/Contents.swift
@@ -1,5 +1,5 @@
 import UIKit
-
+import Foundation
 //input 1
 //for loop how many 1 in input & how many 2 in input
 //combine count of 1 and "1" + count of 2 and 2
@@ -42,4 +42,59 @@ func gernerateSeq(input:Int) -> [String] {
         tmp = str
     }
     return result
+}
+
+//Prime Order Priortization
+//Amazon is planning to release a new order prioritization algorithm to optimize fulfilling Prime deliveries on time. All orders (Prime or otherwise) are given an alphanumeric ID code. However, Prime orders are given additional metadata that consists of a space-delimited list of lowercase English letters, whereas non-Prime orders are given metadata that consists only of a space-delimited string of positive integers. Each order is therefore defined as their alphanumeric id code, followed by a space, followed by their space-delimited metadata.
+//You have been tasked with sorting a list of all orders in the order queue to assist in prioritization of fulfillment. They should be sorted according to the following order:
+//1. The Prime orders should be returned first, sorted by lexicographic sort of the alphabetic metadata.
+//2. Only in the case of ties, the identifier should be used as a backup sort.
+//3. The remaining non-Prime orders must all come after, in the original order they were given in the input.
+//Write a function or method to return a list of strings representing the correctlyprioritized orders according to this system.
+//Output:Return a list of strings representing the correctly prioritized orders according to this system
+//Constraints Os number of orders $ 103
+//Note
+//The order identifier consists of only lower case English characters and numbers. Sorting for tiebreaks should use ASCII value - as an example, the order identifier 'a1' should come before the order identifier 'aa'
+//Examples
+/*
+Input: orderList=
+[zld 93 12]
+[fp kindle book]
+[10a echo show]
+[17g 12 25 6]
+[ab1 kindle book]
+[125 echo dot second generation]
+*//*
+Output;
+[125 echo dot second generation]
+[10a echo show]
+[ab1 kindle book]
+[fp kindle book]
+[zld 93 12]
+[17g 12 25 6]
+*/
+
+func primeOrderPriortization(input:[String]) -> [String] {
+    //detect non-prime and move them to a specific array
+    //sort remain element
+    var input = input
+    var nonPrimeArr:[String] = []
+    for i in 0..<input.count {
+        let splitInput = input.split(separator: " ")
+        let scanner = Scanner(string: String(describing: splitInput[1]))
+        if scanner.scanInt() != nil {
+            nonPrimeArr.append(input.remove(at: i))
+        }
+    }
+    
+    input.sorted { x, y in
+        var splitX = x.split(separator: " ")
+        var splitY = y.split(separator: " ")
+    }
+    
+    
+    
+    
+    
+    return []
 }
