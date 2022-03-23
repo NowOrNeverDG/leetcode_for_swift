4443bac9f6ba575a1f65001079de3d53a4d2a154
Ge Ding <447405890@qq.com>
2022-03-22 23:58:39 -0400

    update interview questions

--- Algorithm/Two-Pointers.playground/Contents.swift
+++ Algorithm/Two-Pointers.playground/Contents.swift
@@ -1,22 +1,27 @@
 import UIKit
-//167. Two Sum II - Input Array Is Sorted
-//Input: numbers = [2,7,11,15], target = 9
-//Output: [1,2]
-func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+import Darwin
+//345. Reverse Vowels of a String
+//Input: s = "hello"
+//Output: "holle"
+func reverseVowels(_ s: String) -> String {
+    var sArr = Array(s)
     var head = 0
-    var tail = 1
-    while (head < numbers.count) {
-        if tail >= numbers.count || target < numbers[head] + numbers[tail] {
-            head += 1
-            tail = head + 1
-            continue
+    var tail = sArr.count-1
+    
+    while (head < tail) {
+        if "aeiouAEIOU".contains(sArr[head]) {
+            while (!"aeiouAEIOU".contains(sArr[tail]) && head < tail) {
+                tail -= 1
+            }
+            
+            sArr.swapAt(head, tail)
+            tail -= 1
         }
-        if target == numbers[head] + numbers[tail] {
-            return [head+1,tail+1]
-        }
-        tail += 1
+        head += 1
     }
-    return []
+    return sArr.reduce("") { partialResult, char in
+        return partialResult + String(char)
+    }
 }
 
 //633. Sum of Square Numbers
@@ -24,8 +29,40 @@ func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
 //Output: true
 //Explanation: 1 * 1 + 2 * 2 = 5
 func judgeSquareSum(_ c: Int) -> Bool {
-   
+    var head = 0
+    var tail = Int(Double(c).squareRoot())
+    
+    while (head <= tail) {
+        if c > head*head + tail*tail {
+            head += 1
+        } else if c < head*head + tail*tail {
+            tail -= 1
+        } else {
+            return true
+        }
+    }
+    return false
 }
+
+//167. Two Sum II - Input Array Is Sorted
+//Input: numbers = [2,7,11,15], target = 9
+//Output: [1,2]
+func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+    var head = 0
+    var tail = numbers.count - 1
+    
+    while(head < tail) {
+        if target > numbers[head] + numbers[tail] {
+            head += 1
+        } else if target < numbers[head] + numbers[tail] {
+            tail -= 1
+        } else {
+            return [head+1,tail+1]
+        }
+    }
+    return []
+}
+
 
 
 
