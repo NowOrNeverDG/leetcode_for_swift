4a262aed8041035f9397a32845d34a1e65dd2ed9
Ge Ding <447405890@qq.com>
2022-04-12 23:42:41 -0400

    update DFS

--- Algorithm/Sliding_Window.playground/Contents.swift
+++ Algorithm/Sliding_Window.playground/Contents.swift
@@ -6,26 +6,26 @@ import Darwin
 func longestSubarray(_ nums: [Int], _ limit: Int) -> Int {
     if nums.count == 0 {return 0}
     var head = 0
-    var tail = 0
+    var tail = 1
     
     var minV = -1
     var maxV = -1
     var length = 0
-    while (head < nums.count) {
-        minV = min(minV, nums[head], nums[tail])
-        maxV = max(maxV, nums[head], nums[tail])
-        
-        if maxV - minV > limit {
-            head += 1
+    while (head < nums.count && tail < nums.count) {
+        if abs(nums[head] - nums[tail]) > 4 {
+            tail += 1
+            head = tail-1
         } else {
-            length = max(length, tail-head)
+            if tail-1 == head {
+                maxV = max[nums[head],nums[tail]
+                minV = min[nums[head],nums[tail]
+                tail += 1
+            }
+                           minV = min(minV,nums[tail])
+                           maxV = max(maxV, nums[tail])
+                           length = tail - head
+                           tail += 1
         }
-        tail += 1
-    }
-    
-    func absoluteValue(first: Int, second: Int) -> Int{
-        let res = first - second
-        return res < 0 ? -res : res
     }
     return length
 }
